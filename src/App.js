5068451ac4a6bb7a6056bf14edb8fdf1a55f0d74
giancarlo valentin <giancarlo.7991@gmail.com>
2021-02-04 15:52:28 -0500

    init-repo

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import CreateNote from './components/CreateNote';
+import CreateUser from './components/CreateUser';
+import Navigation from './components/Navigation';
+import NoteList from './components/NoteList';
+
+function App() {
+  return (
+    <Router>
+      <Navigation />
+
+      <Route path="/" component={NoteList} exact />
+      <Route path="/edit/:id" component={CreateNote} />
+      <Route path="/create" component={CreateNote} />
+      <Route path="/user" component={CreateUser} />
+
+
+    </Router>
+  );
+}
+
+export default App;
