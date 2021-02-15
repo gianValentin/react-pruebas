e852346f7ca112866de282be0fd431ab3a0b203d
giancarlo <giancarlo.7991@gmail.com>
2021-02-14 20:34:43 -0500

    error

--- src/components/CrudTodo.jsx
+++ src/components/CrudTodo.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import { useDispatch, useSelector } from 'react-redux'
+import React, {useState} from 'react'
+import { useDispatch, useSelector, } from 'react-redux'
 import { agregarTarea, borrarTarea, editarTarea } from '../actions/todoAction'
 
 const CrudTodo = () => {
