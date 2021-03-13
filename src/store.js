09fb803244e23f86621d58a7dfc047038ef9db76
giancarlo <giancarlo.7991@gmail.com>
2021-03-13 08:58:53 -0500

    fix redux - obs

--- src/store.js
+++ src/store.js
@@ -6,7 +6,7 @@ const STORE = createStore(
     rootReducer,
     compose(
         applyMiddleware(thunk),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+        window.__REDUX_DEVTOOLS_EXTENSION__  ? window.__REDUX_DEVTOOLS_EXTENSION__() : f => f
     )
 );
 
